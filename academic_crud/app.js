6e37f6ab39417d3bcb077a1435614f52a5c347f3
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-19 07:58:01 -0300

    feat: implementar rotas da aplicação

--- academic_crud/app.js
+++ academic_crud/app.js
@@ -2,8 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
+const router = require("./routes/router");
+
 const app = express();
 const port = 40000;
+
+app.use(bodyParser.urlencoded({ extended: false, }));
+app.use(express.json());
+
+app.use(router);
 
 // app.use(router);
 app.listen(port, () => {
