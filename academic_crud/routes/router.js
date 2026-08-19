72b88e38a2499503c10981b6bdb69d024e1be560
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-19 10:23:05 -0300

    feat: implementar autenticação com JWT

--- academic_crud/routes/router.js
+++ academic_crud/routes/router.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const routerApp = express.Router();
 
+const appLogin = require("../apps/login/controller/ctlLogin");
+
 // middleware that is specific to this router
 routerApp.use((req, res, next) => {
   next();
@@ -11,7 +13,10 @@ routerApp.get("/", (req, res) => {
   res.send("Olá mundo!");
 });
 
-//@ Rotas de Alunos
-//@ Rotas de Cursos
+// Rotas de Alunos
+// Rotas de Cursos
 // Rota Login
+routerApp.post("/Login", appLogin.Login);
+routerApp.post("/Logout", appLogin.Logout);
+
 module.exports = routerApp;
