91e0e8108ca6933009952317930a3355a5c9f114
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-19 07:54:21 -0300

    fix: retirar rotas não configuradas

--- academic_crud/app.js
+++ academic_crud/app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
-// const router = require("./routes/router");
 
 const app = express();
 const port = 40000;
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.json());
 
 // app.use(router);
 app.listen(port, () => {
