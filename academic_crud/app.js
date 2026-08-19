4bf56d7efc38d74b06f21305ab0372146112f37a
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-08-19 08:08:42 -0300

    feat: implementar configuração de CORS

--- academic_crud/app.js
+++ academic_crud/app.js
@@ -10,6 +10,19 @@ const port = 40000;
 app.use(bodyParser.urlencoded({ extended: false, }));
 app.use(express.json());
 
+//Configurar o CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Acess-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  if(req.method === "OPTIONS"){
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
 app.use(router);
 
 // app.use(router);
