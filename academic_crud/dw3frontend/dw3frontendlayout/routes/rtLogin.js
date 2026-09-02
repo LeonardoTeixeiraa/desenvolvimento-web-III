1a608a62621c3e02e1e84e6e39984472c81c2edd
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-09-02 11:25:38 -0300

    mover app.js e views para o diretório dw3frontend

--- academic_crud/dw3frontend/dw3frontendlayout/routes/rtLogin.js
+++ academic_crud/dw3frontend/dw3frontendlayout/routes/rtLogin.js
@@ -4,7 +4,7 @@ router.get("/", function (req, res) {
   res.render("login", {
     title: "Login",
     showNavbar: false,
-    servidorDw3: process.env.SERVIDOR_DW3, // Linha adicionada
+    servidorDw3: process.env.SERVIDOR_DW3,
   });
 });
 router.post("/", function (req, res) {
