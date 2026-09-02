d64c47ea820065c750bc2c52b3347c10bf587a2b
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-09-02 10:05:54 -0300

    feat: implementar rota do login no front-end

---
+++ academic_crud/dw3frontend/dw3frontendlayout/routes/rtLogin.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var router = express.Router();
+router.get("/", function (req, res) {
+  res.render("login", {
+    title: "Login",
+    showNavbar: false,
+  });
+});
+router.post("/", function (req, res) {
+  res.redirect("/home");
+});
+module.exports = router;
