649c5c2475d9508f78cb67e2d98b37b4f9e83d7c
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-09-02 09:09:06 -0300

    atualizar o app.js

--- academic_crud/dw3frontend/dw3frontendlayout/app.js
+++ academic_crud/dw3frontend/dw3frontendlayout/app.js
@@ -16,3 +16,23 @@ nunjucks.configure(viewsPath, {
   express: app,
   noCache: app.get("env") === "development",
 });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+app.use(function (err, req, res, next) {
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.status(err.status || 500);
+  res.render("error", { title: "Erro" });
+});
+app.listen(port, () => {
+  console.log(`App listening at port ${port}`);
+});
+
