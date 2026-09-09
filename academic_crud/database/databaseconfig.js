fba729d4a81f550cdbfbb95026e3e57ec2414721
LeonardoTeixeiraa <leonardo.teixeira@aluno.ifsp.edu.br>
2026-09-09 07:43:20 -0300

    feat: adicionar teste de banco de dados

--- academic_crud/database/databaseconfig.js
+++ academic_crud/database/databaseconfig.js
@@ -1,10 +1,25 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-user: process.env.DB_USER,
-host: process.env.DB_HOST,
-database: process.env.DB_NAME,
-password: process.env.DB_PASS,
-port: process.env.DB_PORT,})
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: process.env.DB_PORT,
+});
 module.exports = {
-query: (text, params) => pool.query(text, params),
+  query: (text, params) => pool.query(text, params),
+};
+
+pool.connect((err, client, release) => {
+  if (err) {
+    console.error("❌ Erro ao conectar ao banco:", err.message);
+    return;
+  }
+
+  console.log("✅ Conexão com o banco de dados estabelecida!");
+  release();
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
 };
